240d4e5dd1d04938007e75b8ee67c7406c0e698b
RyU <realriu@gmail.com>
2014-01-19 16:37:30 +0100

    + support for multiple led-stripes

--- server/config.py
+++ server/config.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #! /usr/bin/env python
 
 
@@ -26,33 +25,3 @@ SERVER_PORT = 4321
 ENABLE_XBMC_REMOTE = 0
 XBMC_HOST = "127.0.0.1"
 XBMC_PORT = 80
-=======
-#! /usr/bin/env python
-
-
-#GPIO pins for RGB
-RED_PIN_1 = 5
-GREEN_PIN_1 = 2
-BLUE_PIN_1 = 6
-
-# this value indicates the minimum value of the sum of
-# red, green and blue channels can be set to before the LEDs start blinking
-MIN_VALUE = 0.00
-
-# this is the minimum value threads wait between their processing intervals
-# increase this value to get more cpu time for other programs
-# decrease this value, to get smoother fading
-# don't set this value lower or equal 0!!!
-DELAY = 0.01
-
-
-
-#server port
-SERVER_PORT = 4321
-
-
-#Data for XBMC remote control
-ENABLE_XBMC_REMOTE = 0
-XBMC_HOST = "127.0.0.1"
-XBMC_PORT = 80
->>>>>>> master
