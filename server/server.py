c2dbccd754e20d7fdd588e7719209857022fcfa4
RyU <realriu@gmail.com>
2014-02-09 13:56:41 +0100

    ~ replaced command queue by a own pointer design

--- server/server.py
+++ server/server.py
@@ -40,91 +40,136 @@ class CommandThread(threading.Thread):
         self.name = name
         self.cmd = cmd
         self.state = constants.CMD_STATE_INIT
+        self.runtime = 0
+        self.timer = None
+        self.next = None
 
     #method, which is executed when the .start() method of the thread is called
     def run(self):
         log.l("Starting Thread: " + self.name, log.LEVEL_START_STOP_THREADS)
         self.state = constants.CMD_STATE_STARTED
 
+        #a command is alsways followed by a time (secs) after the command expires
+        #set runtime to '0' or below to run the command infinite
+        self.runtime = float(self.cmd[1])
+        if self.runtime > 0.0:
+            #set the timer for the given runtime
+            self.timer = threading.Timer(self.runtime, self.stop())
+
         #decide which command should be executed
 
 # FADE
         if self.name == 'fade':
-            corefunctions.fade(self, float(self.cmd[1]), led.Color(self.cmd[2]), led.Color(self.cmd[3]) if len(self.cmd) > 3 else None)
+            corefunctions.fade(self, float(self.cmd[2]), led.Color(self.cmd[3]), led.Color(self.cmd[4]) if len(self.cmd) > 4 else None)
 
 # RANDOM FADE
         elif self.name == "rf":
-            corefunctions.startRandomFade(self, float(self.cmd[1]), float(self.cmd[2]), float(self.cmd[3]) if len(self.cmd) > 3 else None, float(self.cmd[4]) if len(self.cmd) > 4 else None)
+            corefunctions.startRandomFade(self, float(self.cmd[2]), float(self.cmd[3]), float(self.cmd[4]) if len(self.cmd) > 4 else None, float(self.cmd[5]) if len(self.cmd) > 5 else None)
 
 # PULSE
         elif self.name == "pulse":
-            if len(self.cmd) > 3:
-                pulse.startPulse(float(self.cmd[1]), led.Color(self.cmd[2]), led.Color(self.cmd[3]))
+            if len(self.cmd) > 4:
+                pulse.startPulse(float(self.cmd[2]), led.Color(self.cmd[3]), led.Color(self.cmd[4]))
             else:
-                pulse.startPulse(float(self.cmd[1]), led.Color(self.cmd[2]))
+                pulse.startPulse(float(self.cmd[2]), led.Color(self.cmd[3]))
 # JUMP
         elif self.name == "jump":
-            if len(self.cmd) > 3:
-                jump.startJump(float(self.cmd[1]), led.Color(self.cmd[2]), led.Color(self.cmd[3]))
+            if len(self.cmd) > 4:
+                jump.startJump(float(self.cmd[2]), led.Color(self.cmd[3]), led.Color(self.cmd[4]))
             else:
-                jump.startJump(float(self.cmd[1]), led.Color(self.cmd[2]))
+                jump.startJump(float(self.cmd[2]), led.Color(self.cmd[3]))
 
 
-        self.state = constants.CMD_STATE_EXPIRED
+        self.stop()
         log.l("Exiting:" + self.name, log.LEVEL_START_STOP_THREADS)
 
     #stops this thread, disregarding when its expiration should be
     def stop(self):
         log.l("stopping " + self.name, log.LEVEL_START_STOP_THREADS)
+
+        #stop timer
+        if self.timer is not None:
+            self.timer.cancel()
+            self.timer = None
+
         if(self.state != constants.CMD_STATE_EXPIRED):
             self.state = constants.CMD_STATE_EXPIRED
+        global CMDQ
+        CMDQ.notify()
 
 
-class QueueThread(threading.Thread):
+class CommandQueue(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
-        self.lock = threading.Condition()
-        self.Q = Queue.Queue()
+        self.lock = threading.Semaphore()
+        self.monitor = threading.Event()
         self.current = None
+        self.startCMD = None
         self.alive = 1
+
+        self.monitor.clear()
 
     def run(self):
         while self.alive:
-            if self.current is None:
-                self.current = self.deq()
-                if self.current.state == constants.CMD_STATE_INIT:
-                    self.current.start()
+            while self.startCMD is None and self.alive:
+                self.monitor.wait()
+            if self.alive:
+                self.lock.acquire()
+                if self.current is None:
+                    self.current = self.startCMD
+                    if self.current.state == constants.CMD_STATE_INIT:
+                        self.current.start()
+                else:
+                    if self.current.next is not None and self.current.state == constants.CMD_STATE_EXPIRED:
+                        self.current = self.current.next
+                        self.current.start()
+                    else:
+                        self.current = None
+                        self.startCMD = None
 
-            while self.current.state == constants.CMD_STATE_STARTED:
-                pass #TODO: wait command queue
-
-            self.current = None
+                self.lock.release()
+                while self.current is not None and self.current.state == constants.CMD_STATE_STARTED and self.alive:
+                    self.monitor.wait()
 
     def stop(self):
         self.lock.acquire()
+        self.startCMD = None
         if self.current is not None:
             self.current.stop()
             self.current = None
+
         self.alive = 0
+        self.notify()
         self.lock.release()
 
     def enq(self, cmd):
         self.lock.acquire()
-        self.Q.put_nowait(cmd)
+        #if no command is running, take cmd as main command
+        if self.startCMD is None:
+            self.startCMD = cmd
+
+        #attach to last if possible
+        lastCMD = self.startCMD.next
+        while lastCMD is not None and lastCMD.next is not None and lastCMD.next is not self.startCMD:
+            lastCMD = lastCMD.next
+
+        if lastCMD.next is self.startCMD:
+            if lastCMD.runtime == 0:
+                lastCMD.next = cmd
+                cmd.next = self.startCMD
+            elif lastCMD.runtime < 0:
+                self.current.stop()
+                self.startCMD = cmd
+        elif lastCMD.runtime > 0:
+            lastCMD.next
+
+        self.notify()
         self.lock.release()
 
-    def deq(self):
-        self.lock.acquire()
-        cmdT = None
-        if not self.Q.empty():
-            cmdT = self.Q.get_nowait()
-        self.lock.release()
-        return cmdT
+    def notify(self):
+        self.monitor.set()
 
-    def pulse(self):
-        pass #TODO: wake up the command queue
-
-CMDQ = QueueThread()
+CMDQ = CommandQueue()
 
 
 
@@ -161,6 +206,15 @@ def readcommands(threadName, intervall):
                 if command[0] == "cc":
                     log.l(command, log.LEVEL_COMMAND_CC)
                     led.setColor(led.Color(command[1]))
+
+                elif command[0] == "loop":
+                    log.l(command, log.LEVEL_COMMANDS)
+                    lastCMD = CMDQ.startCMD.next
+                    while lastCMD is not None and lastCMD.next is not None and lastCMD.next is not CMDQ.startCMD:
+                        lastCMD = lastCMD.next
+                    if lastCMD.next is not CMDQ.startCMD:
+                        lastCMD.next = CMDQ.startCMD
+
                 else:
                     log.l(command, log.LEVEL_COMMANDS)
                     ID += 1
